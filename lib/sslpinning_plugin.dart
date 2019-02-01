397664afa2acd54d83f1e6e8403fdb624cf75199
us3soap <kevin.valette@gmail.com>
2019-02-01 15:54:00 +0100

    Changelog

---
+++ lib/sslpinning_plugin.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+
+enum SHA {SHA1, SHA256}
+
+class SslPinningPlugin {
+  static const MethodChannel _channel = const MethodChannel('sslpinning_plugin');
+
+  // Création d'un singleton pour le plugin
+  static final SslPinningPlugin _sslPinning = new SslPinningPlugin._internal();
+
+  factory SslPinningPlugin() => _sslPinning;
+
+  SslPinningPlugin._internal() {
+    _channel.setMethodCallHandler(_platformCallHandler);
+  }
+
+  static Future<String> check({ String serverURL, Map<String, String> headerHttp, SHA sha, List<String> allowedSHAFingerprint, int timeout }) async {
+    final Map<String, dynamic> params = <String, dynamic>{
+      "url" : serverURL,
+      "headers" : headerHttp,
+      "type": sha.toString().split(".").last,
+      "fingerprints" : allowedSHAFingerprint,
+      "timeout" : timeout
+    };
+    String resp = await _channel.invokeMethod('check', params);
+    return resp;
+  }
+
+  // Ecoute les retours du plugins
+  Future _platformCallHandler(MethodCall call) async {
+    print("_platformCallHandler call ${call.method} ${call.arguments}");
+  }
+}
