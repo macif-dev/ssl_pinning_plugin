fbbb259482a4a7abeb9d0282d6f694d45c6ffd3b
Kevin Valette <kevin.valette@gmail.com>
2019-02-01 18:16:03 +0100

    Delete SslPinningPlugin.m

--- ios/Classes/SslPinningPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "SslpinningPlugin.h"
-#import <sslpinning_plugin/sslpinning_plugin-Swift.h>
-
-@implementation SslpinningPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftSslpinningPlugin registerWithRegistrar:registrar];
-}
-@end
